edcd5a639d516f4cd938e1adc07d0739c694fe7d
Janmax <tomix.max123@o2.pl>
2018-10-17 11:08:27 +0000

    finish toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   #validates :user_id, presence: true
   validates :content, length: { maximum: 140 }
+  #validates :name,  presence: true
 end
